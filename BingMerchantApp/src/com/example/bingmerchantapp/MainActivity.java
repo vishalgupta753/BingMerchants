06d65af8cc33dafae7f5f6ec8276985d738dcd49
visgupta <visgupta@visgupta-lp.fareast.corp.microsoft.com>
2014-07-29 02:27:09 +0530

    new change

--- BingMerchantApp/src/com/example/bingmerchantapp/MainActivity.java
+++ BingMerchantApp/src/com/example/bingmerchantapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.bingmerchantapp;
 
+import com.example.bingmerchantapp.AppConstants.ConsumerStatus;
+import com.example.bingmerchantapp.AppConstants.MerchantStatus;
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -8,40 +11,66 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-
 public class MainActivity extends ActionBarActivity {
-	
+
 	public static Context context;
+	public static ConsumerStatus CurrentUserConsumerStatus;
+	public static MerchantStatus CurrentUserMerchantStatus;
+	public static String CurrentUserId;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		MainActivity.context = getApplicationContext();
+		setContentView(R.layout.activity_main);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void gotoMerchantScreen (View view)
-    {
-    	Intent intent = new Intent(this, MerchantSignup.class);
-    	startActivity(intent);
-    }
+	public void gotoMerchantScreen(View view) {
+		Intent intent = null;
+		if (AppUtils.StringIsNullOrEmpty(CurrentUserId) == false) {
+			intent = new Intent(this, MerchantSignup.class);
+		} else {
+			AppUtils.SetCuurentUserMerchantStatus();
+			if (CurrentUserMerchantStatus == MerchantStatus.NewMarchant) {
+				intent = new Intent(this, MerchantSignup.class);
+			} else {
+				intent = new Intent(this, MerchantSignup.class);
+			}
+		}
+		startActivity(intent);
+	}
+	
+	public void gotoConsumerScreen(View view) {
+		Intent intent = null;
+		if (AppUtils.StringIsNullOrEmpty(CurrentUserId) == false) {
+			intent = new Intent(this, MerchantSignup.class);
+		} else {
+			AppUtils.SetCuurentUserConsumerStatus();
+			if (CurrentUserConsumerStatus == ConsumerStatus.NewConsumer) {
+				intent = new Intent(this, MerchantSignup.class);
+			} else {
+				intent = new Intent(this, MerchantSignup.class);
+			}
+		}
+		startActivity(intent);
+	}
 }
---
+++ BingMerchantApp/src/com/example/bingmerchantapp/data/Merchant.java
@@ -0,0 +1,71 @@
+package com.example.bingmerchantapp.data;
+
+import java.util.ArrayList;
+
+public class Merchant {
+	
+	private String MerchantId;
+	private String Name;
+	private String Phone;
+	private String Address;
+	private String BusinessName;
+	private ArrayList<String> Services;
+	private String GeoLocation;
+	
+	public Merchant(String merchantId, String name, String phone,
+			String address, String businessName,String services, String geoLocation) {
+		super();
+		MerchantId = merchantId;
+		Name = name;
+		Phone = phone;
+		Address = address;
+		BusinessName = businessName;
+		Services = new ArrayList<String>();
+		for (String str : services.split(",")) {
+			Services.add(str);
+		}
+		GeoLocation = geoLocation;
+	}
+	public String getMerchantId() {
+		return MerchantId;
+	}
+	public void setMerchantId(String merchantId) {
+		MerchantId = merchantId;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getBusinessName() {
+		return BusinessName;
+	}
+	public void setBusinessName(String businessName) {
+		BusinessName = businessName;
+	}
+	public ArrayList<String> getServices() {
+		return Services;
+	}
+	public void setServices(ArrayList<String> services) {
+		Services = services;
+	}
+	public String getGeoLocation() {
+		return GeoLocation;
+	}
+	public void setGeoLocation(String geoLocation) {
+		GeoLocation = geoLocation;
+	}
+}
