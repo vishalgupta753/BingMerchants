cf868b9794d83a82817795d85c40c2b70c792d03
visgupta <visgupta@visgupta-lp.fareast.corp.microsoft.com>
2014-07-28 23:22:35 +0530

    changes regarding appp status

--- BingMerchantApp/src/com/example/bingmerchantapp/AppUtils.java
+++ BingMerchantApp/src/com/example/bingmerchantapp/AppUtils.java
@@ -1,12 +1,58 @@
 package com.example.bingmerchantapp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class AppUtils {
-	
-	public static String GetLocalSavedUserId()
-	{
+
+	public static String GetLocalSavedUserId() {
 		String userId = null;
-		
+		InputStreamReader inputStreamReader = null;
+
+		File file = MainActivity.context
+				.getFileStreamPath(AppConstants.FileName);
+		if (file.exists()) {
+			try {
+				FileInputStream in = MainActivity.context
+						.openFileInput(AppConstants.FileName);
+				inputStreamReader = new InputStreamReader(in);
+				BufferedReader bufferedReader = new BufferedReader(
+						inputStreamReader);
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					sb.append(line);
+				}
+			} catch (Exception e) {
+
+			} finally {
+				if(inputStreamReader != null)
+				{
+					try {
+						inputStreamReader.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
 		return userId;
 	}
-
+	
+	public static Boolean checkIfMerchantExits(String mid)
+	{
+		// Use server apis here
+		return false;
+	}
+	
+	public static Boolean checkIfConsumerExits(String cid)
+	{
+		// Use server apis here
+		return false;
+	}
 }
--- BingMerchantApp/src/com/example/bingmerchantapp/MainActivity.java
+++ BingMerchantApp/src/com/example/bingmerchantapp/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.bingmerchantapp;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	public static Context context;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
     }
 
